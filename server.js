b84cc8b34ecc295fc55fb5cc32ff27c5c99ce7d6
Jeff Gerber <jallengerber@gmail.com>
2015-09-09 14:23:52 -0700

    added server.js

--- server.js
+++ server.js
@@ -1,63 +1,50 @@
 'use strict';
 
 var http = require('http');
-//var url = require('url');
-//var net = require('net');
 
 var server = http.createServer(function(req,res) {
-
-
-  if (req.url ==='/time') {
-    var time = Date.now();
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+  var path = req.url.split('/');
+  if (path[1] ==='greet') {
+    if (req.method === 'GET') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('Hello ' + path[2]);
+    return res.end();
+    }
+    else if (req.method === 'POST') {
+      req.on('data', function(data) {
+        var parsed = JSON.parse(data);
+        res.writeHead(200, {
+          'Content-Type': 'text/plain'
+        });
+        res.write(JSON.stringify({greet: 'Hello ' + parsed.Name }));
+        return res.end();
+      });
+    }
+  }
+  else if (path[1] ==='time') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    var time = new Date();
     res.write(time.toString());
     return res.end();
   }
-
-  if (req.url === '/greet' && req.method === 'POST') {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    req.on('data', function(data) {
-      var parsed = JSON.parse(data);
-      console.log(parsed);
-    res.write(parsed.Name);
-    return res.end();
-    });
+  else {
+    res.writeHead(404, {
+    'Content-Type': 'text/plain'
+  });
+  res.write('page not found');
+  res.end();
   }
-
-
-
-
-  var path = req.url.toString();   //this code block is blocking the other block
-  var name = path.substring(7);
-  var reg = /(\/greet\/)[a-zA-Z0-9]*/;
-  if (req.url.match(path)) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello ' + name);
-    return res.end();
-  }
-
-
-
-
-
-
-
-
-  // if (req.url ==='/greet') {
-  //   res.writeHead(200, {'Content-Type': 'text/plain'});
-  //   res.write('Hello World');
-  //   return res.end();
-  // }
-
-  // res.writeHead(404, {
-  //   'Content-Type': 'text/plain'
-  // });
-
-  // res.write('page not found');
-  // res.end();
-
 });
 
-server.listen(3000, function() {
+  server.listen(3000, function() {
   console.log("server Listening on port 3000");
 });
+
+
+
+
+
